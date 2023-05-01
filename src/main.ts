145a703d70c1374d813e062371cb86edd7f42c55
James Moore <th3darkrose@gmail.com>
2023-05-01 09:23:17 -0400

    final version

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,43 @@
-import * as core from '@actions/core';
+import * as core from '@actions/core'
 import * as github from '@actions/github'
-import * as fs from 'fs';
+import * as fs from 'fs'
 
 async function run(): Promise<void> {
     try {
-        const lastReleaseName = core.getInput('last_release_name');
-        const changelogFile = core.getInput('changelog_file');
-        const token = core.getInput('github_token');
-
-        const octokit = github.getOctokit(token);
-
-        const repo = github.context.repo;
-        const releaseResponse = await octokit.rest.repos.getLatestRelease({
-            owner: repo.owner,
-            repo: repo.repo
-        });
-        //const lastReleaseTag = releaseResponse.data.tag_name;
+        const releaseVersion = core.getInput('release_version')
+        const changelogFile = core.getInput('changelog_file') || 'changelog.md'
+        const token = core.getInput('github_token')
+        const octokit = github.getOctokit(token)
+        const repo = github.context.repo
+        const releaseResponse = await octokit.rest.repos.getLatestRelease(repo)
 
         const commitsResponse = await octokit.rest.repos.listCommits({
             owner: repo.owner,
             repo: repo.repo,
             since: releaseResponse.data.created_at
-        });
-        const commitMessages = commitsResponse.data.map((commit) => commit.commit.message);
+        })
+        const commits = commitsResponse.data.map(
+            commit => commit.commit.message
+        )
 
-        const logRegex = /<log>(.*?)<\/log>/gs;
-        const changelogText = commitMessages
+        const logRegex = /(.*?)<\/log>/gs
+        const changelogText = commits
             .reduce<string[]>((acc, message) => {
-                console.log(`message: ${message}`)
-                const match = message.match(logRegex);
-                console.log(`match: ${match}`);
-                if (match) {
-                    console.log(`match[1]: ${match[1]}`)
-                    console.log(`groups: ${match.groups?[1]:String}`)
-                }
+                logRegex.lastIndex = 0
+                const match = logRegex.exec(message)
                 if (match && match[1]) {
-                    acc.push(match[1]);
+                    acc.push(match[1].trim())
                 }
-                console.log(`acc: ${acc}`)
-                return acc;
+                return acc
             }, [])
-            .join('');
+            .join('\n')
 
-        const changelog = fs.readFileSync(changelogFile, 'utf8');
-        const newChangelog = `## ${lastReleaseName}\n\n${changelogText}\n\n${changelog}`;
-        console.log(`Last release:  ${lastReleaseName}`);
-        console.log(`Changelog text: ${changelogText}`);
-        console.log(`changelog: ${changelog}`);
-        console.log(`new changelog: ${newChangelog}`);
-        fs.writeFileSync(changelogFile, newChangelog);
-
-        core.setOutput('changelog', newChangelog);
+        const changelog = fs.readFileSync(changelogFile, 'utf8')
+        const newChangelog = `# -----{ ${releaseVersion} }-----\n\n${changelogText}\n\n${changelog}`
+        fs.writeFileSync(changelogFile, newChangelog)
+        core.setOutput('changelog', newChangelog)
+        console.log('New changelog generated:')
+        console.log(newChangelog)
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
     }
